06d2fb6242d5248bdf38386be7e904aaa0d4ef5f
Adolfo Builes <builes.adolfo@gmail.com>
2019-04-10 08:24:39 -0500

    Add test for enum.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import { Hyper, UnsignedHyper } from 'js-xdr'
+import { Hyper, UnsignedHyper, Enum } from 'js-xdr'
 
 function serializeHyper(value: Hyper): string {
   return value.toString()
 }
 
+function serializeEnum({ name }: Enum): string {
+  return name
+}
+
 function serialize(xdrType: any, value: any) {
   if (value instanceof Hyper || value instanceof UnsignedHyper) {
     return serializeHyper(value)
+  } else if (value instanceof Enum) {
+    return serializeEnum(value)
   } else {
     return value
   }
