3b67d430992634808c0b87720309add41d7b0553
Adolfo Builes <builes.adolfo@gmail.com>
2019-04-11 10:00:33 -0500

    Add serializeStruct.

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ function serializeEnum({ name }: Enum): string {
   return name
 }
 
-function serialize(xdrType: any, value: any) {
+function serialize(xdrType: any, value: any): any {
   if (value instanceof Hyper || value instanceof UnsignedHyper) {
     return serializeHyper(value)
   } else if (value instanceof Enum) {
@@ -28,9 +28,13 @@ function serialize(xdrType: any, value: any) {
   }
 }
 
-export function toJSON(types: object, structType: StructConstructable, struct: Struct): any {
+function serializeStruct(structType: StructConstructable, struct: Struct): any {
   return structType._fields.reduce(function(json, [name, type]) {
     json[name] = serialize(type, struct._attributes[name])
     return json
-  }, {})
+  }, {});
+}
+
+export function toJSON(types: object, structType: StructConstructable, struct: Struct): any {
+  return serializeStruct(structType, struct);
 }
