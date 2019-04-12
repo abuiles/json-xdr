5d4ef833609681decf30e60f1c768303d5ee0c0e
Adolfo Builes <builes.adolfo@gmail.com>
2019-04-12 18:17:18 -0500

    Add deserialization support for struct with ints.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Struct } from "js-xdr";
+import { deserializeStruct } from "./deserializer";
 import { IStructConstructable, serializeStruct } from "./serializer";
 
 export function toJSON(types: object, struct: Struct): any {
   return serializeStruct(struct);
 }
 
-export function toXDR(structType: IStructConstructable, json: object) {
-  return json;
+export function toXDR(structType: IStructConstructable, payload: object) {
+  return deserializeStruct(structType, payload);
 }
