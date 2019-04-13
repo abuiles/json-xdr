ab15c70ca2b1d36d18ea9e3217187f1ce2126309
Adolfo Builes <builes.adolfo@gmail.com>
2019-04-12 19:47:49 -0500

    Add deserialization support for Hyper values.

--- src/deserializer.ts
+++ src/deserializer.ts
@@ -1,16 +1,30 @@
-import { Struct } from "js-xdr";
+import { Hyper, Struct, UnsignedHyper } from "js-xdr";
 // TODO: Move this to a types file
 import { IStructConstructable } from "./serializer";
 
+function toHyper(value: string): Hyper {
+  return Hyper.fromString(value);
+}
+
+function toUnsignedHyper(value: string): UnsignedHyper {
+  return UnsignedHyper.fromString(value);
+}
+
 export default function toXDR(xdrType: any, value: any): any {
-  return value;
+  if (xdrType === Hyper) {
+    return toHyper(value);
+  } else if (xdrType === UnsignedHyper) {
+    return toUnsignedHyper(value);
+  } else {
+    return value;
+  }
 }
 
 export function deserializeStruct(structConstructor: IStructConstructable, payload: object): Struct {
   const attrs = {};
 
-  structConstructor._fields.forEach(([name, type]) => {
-    attrs[name] = toXDR(type, payload[name]);
+  structConstructor._fields.forEach(([name, objectType]) => {
+    attrs[name] = toXDR(objectType, payload[name]);
   });
 
   return new structConstructor(attrs);
