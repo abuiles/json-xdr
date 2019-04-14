14295c9bfa7de223a0777acbfa7ad7ddebab57ed
Adolfo Builes <builes.adolfo@gmail.com>
2019-04-14 13:26:25 -0500

    Test more data.

--- src/integration.test.ts
+++ src/integration.test.ts
@@ -6,34 +6,42 @@ const STELLAR_HOST = "https://horizon.stellar.org";
 const server = new StellarSdk.Server(STELLAR_HOST);
 
 describe("Integration", () => {
-  if (process.env.RUN_INTEGRATION) {
-    test("serializing and deserializing Stellar transaction result", async () => {
-      const testTransaction = (transaction) => {
-        console.log("serializing and deserializing transaction: ", transaction.id);
+  test("serializing and deserializing Stellar transaction result", async () => {
+    if (!process.env.RUN_INTEGRATION) { return; }
 
-        let xdr = StellarSdk.xdr.TransactionResult.fromXDR(transaction.result_xdr, "base64");
-        let json = toJSON(xdr);
+    const testTransaction = (transaction) => {
+      console.log("serializing and deserializing transaction: ", transaction._links.self.href);
 
-        let xdrCopy = toXDR(StellarSdk.xdr.TransactionResult, json);
+      // Read ResultXDR, convert to JSON and back to XDR.
+      let xdr = StellarSdk.xdr.TransactionResult.fromXDR(transaction.result_xdr, "base64");
+      let json = toJSON(xdr);
 
-        expect(transaction.result_xdr).toEqual(xdrCopy.toXDR().toString("base64"));
+      let xdrCopy = toXDR(StellarSdk.xdr.TransactionResult, json);
 
-        xdr = StellarSdk.xdr.TransactionEnvelope.fromXDR(transaction.envelope_xdr, "base64");
-        json = toJSON(xdr);
+      expect(transaction.result_xdr).toEqual(xdrCopy.toXDR().toString("base64"));
 
-        xdrCopy = toXDR(StellarSdk.xdr.TransactionEnvelope, json);
-        expect(transaction.envelope_xdr).toEqual(xdrCopy.toXDR().toString("base64"));
-      };
+      // Read ResultMetaXDR, convert to JSON and back to XDR.
+      xdr = StellarSdk.xdr.TransactionMeta.fromXDR(transaction.result_meta_xdr, "base64");
+      json = toJSON(xdr);
 
-      let transactions = await server.transactions().limit(200).call();
+      xdrCopy = toXDR(StellarSdk.xdr.TransactionMeta, json);
+      expect(transaction.result_meta_xdr).toEqual(xdrCopy.toXDR().toString("base64"));
 
-      transactions = await transactions.next();
-      transactions.records.forEach(testTransaction);
+      // Read TransactionEnvelope, convert to JSON and back to XDR.
+      xdr = StellarSdk.xdr.TransactionEnvelope.fromXDR(transaction.envelope_xdr, "base64");
+      json = toJSON(xdr);
 
-      transactions = await transactions.next();
-      transactions.records.forEach(testTransaction);
-    }, 50000);
-  } else {
-    test.todo("skipping integration tests - use `yarn test:all` to run all test");
-  }
+      xdrCopy = toXDR(StellarSdk.xdr.TransactionEnvelope, json);
+      expect(transaction.envelope_xdr).toEqual(xdrCopy.toXDR().toString("base64"));
+    };
+
+    let transactions = await server.transactions().order("desc").limit(200).call();
+    transactions.records.forEach(testTransaction);
+
+    transactions = await transactions.next();
+    transactions.records.forEach(testTransaction);
+
+    transactions = await transactions.next();
+    transactions.records.forEach(testTransaction);
+  }, 200000);
 });
